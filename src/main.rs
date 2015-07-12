6d6cf68493c593e6fa4e2622d0ad2388ef54f688
Akavall <akavall@gmail.com>
2015-07-11 21:41:43 -0500

    changed params in main

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,11 @@ fn main() {
     let n_crosses = analytics_combs::calc_expected_n_crosses_combinations(0.5, 10);
     println!("expected number of crosses: {}", n_crosses);
 
-    let loss = analytics_combs::calc_loss_combinations(10, 0.8);
+    let loss = analytics_combs::calc_loss_combinations(10, 0.5);
     println!("total loss: {}", loss);
 
     println!("Simulation Result");
-    let sim_loss = simulation_combs::many_simulations(10, 0.8, 100000);
+    let sim_loss = simulation_combs::many_simulations(10, 0.5, 1000000);
     println!("total loss: {}", sim_loss);
 }
 
